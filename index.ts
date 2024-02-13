4285b1a692a26945d00532d4901c86d204c4189e
snomiao <snomiao@gmail.com>
2024-02-13 08:12:04 +0000

    fix(main): logs
    logs

--- index.ts
+++ index.ts
@@ -1,24 +1,38 @@
 import WebSocket from "ws";
-Bun.serve<{ wsc?: WebSocket, headers: Headers }>({
+Bun.serve<{ wsc?: WebSocket; headers: Headers }>({
   fetch(req, server) {
-    if (!server.upgrade(req, { data: { headers: req.headers } }))
-      return fetch(req).catch((err) => new Response(String(err), { status: 500 }))
+    if (!server.upgrade(req, { data: { headers: req.headers } })) {
+      console.log(req.url);
+      return fetch(req).catch(
+        (err) => new Response(String(err), { status: 500 })
+      );
+    }
   },
   websocket: {
     open(ws) {
       const headers = ws.data.headers;
-      const target = 'ws://' + headers.get('host');
-      console.log(target + ' ws open')
-      headers.delete('upgrade') // must delete
-      headers.delete('sec-websocket-key') // must delete
-      headers.delete('sec-websocket-version') // must delete
-      const wsc = (new WebSocket(target, { headers: headers.toJSON() }))
-      wsc.addEventListener('message', (message) => ws.send(message.data as string))
-      wsc.addEventListener('error', (error) => { console.error(error), ws.close(1011) })  // `1011` means the server encountered an error
-      wsc.addEventListener('close', () => { ws.close() })
-      ws.data.wsc = wsc
+      const target = "ws://" + headers.get("host");
+      console.log(`  ${target}`);
+      headers.delete("upgrade"); // must delete
+      headers.delete("sec-websocket-key"); // must delete
+      headers.delete("sec-websocket-version"); // must delete
+      const wsc = new WebSocket(target, { headers: headers.toJSON() });
+      wsc.addEventListener("message", (message) =>
+        ws.send(message.data as string)
+      );
+      wsc.addEventListener("error", (error) => {
+        console.error(error), ws.close(1011);
+      }); // `1011` means the server encountered an error
+      wsc.addEventListener("close", () => {
+        ws.close();
+      });
+      ws.data.wsc = wsc;
     },
-    message(ws, message) { ws.data.wsc?.send(message); },
-    close(ws) { ws.data.wsc?.close() },
+    message(ws, message) {
+      ws.data.wsc?.send(message);
+    },
+    close(ws) {
+      ws.data.wsc?.close();
+    },
   },
 });
